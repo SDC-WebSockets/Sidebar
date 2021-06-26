4f5afa821a4d97148aee6557eae7ddafa7a3af55
kimberly kung <kimberly.kung@gmail.com>
2021-06-26 12:36:48 -0700

    Added new API Call to server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
 const shrinkRay = require('shrink-ray-current');
 const express = require('express');
 const path = require('path');
-const app = express();
-const db = require('../database/database.js');
 const cors = require('cors');
+const db = require('../database/database.js');
+
+const app = express();
 
 app.use(shrinkRay());
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -12,12 +14,12 @@ app.use(cors());
 
 app.get('*.js', (req, res, next) => {
   if (req.header('Accept-Encoding').includes('br')) {
-    req.url = req.url + '.br';
+    req.url += '.br';
     console.log(req.header('Accept-Encoding'));
     res.set('Content-Encoding', 'br');
     res.set('Content-Type', 'application/javascript; charset=UTF-8');
   } else if (req.header('Accept-Encoding').includes('gzip')) {
-    req.url = req.url + '.gz';
+    req.url += '.gz';
     console.log(req.header('Accept-Encoding'));
     res.set('Content-Encoding', 'gzip');
     res.set('Content-Type', 'application/javascript; charset=UTF-8');
@@ -26,13 +28,13 @@ app.get('*.js', (req, res, next) => {
 });
 
 app.get('/price', (req, res) => {
-  console.log("GET request received at /price.");
+  console.log('GET request received at /price.');
   console.log(req.headers);
   db.getPrice(req.query, (err, docs) => {
     if (err) {
       res.send(err);
     } else if (docs[0] === undefined) {
-      res.status(404).send("Database does not contain requested record.");
+      res.status(404).send('Database does not contain requested record.');
     } else {
       res.send(docs[0]);
     }
@@ -40,12 +42,12 @@ app.get('/price', (req, res) => {
 });
 
 app.get('/previewVideo', (req, res) => {
-  console.log("GET request received at /previewVideo.");
+  console.log('GET request received at /previewVideo.');
   db.getPreviewVideo(req.query, (err, docs) => {
     if (err) {
       res.send(err);
     } else if (docs[0] === undefined) {
-      res.status(404).send("Database does not contain requested record.");
+      res.status(404).send('Database does not contain requested record.');
     } else {
       res.send(docs[0]);
     }
@@ -53,12 +55,12 @@ app.get('/previewVideo', (req, res) => {
 });
 
 app.get('/sidebar', (req, res) => {
-  console.log("GET request received at /sidebar.");
+  console.log('GET request received at /sidebar.');
   db.getSidebar(req.query, (err, docs) => {
     if (err) {
       res.send(err);
     } else if (docs[0] === undefined) {
-      res.status(404).send("Database does not contain requested record.");
+      res.status(404).send('Database does not contain requested record.');
     } else {
       res.send(docs[0]);
     }
@@ -66,7 +68,33 @@ app.get('/sidebar', (req, res) => {
 });
 
 app.use('/course', (req, res) => {
-  res.sendFile('index.html', {root: 'public'});
+  res.sendFile('index.html', { root: 'public' });
+});
+
+// Read
+app.get('/sidebar/all', (req, res) => {
+  console.log('GET request received at /sidebar/all.');
+  const fullResponse = {};
+  db.getSidebar(req.query, (err, docs) => {
+    if (err) {
+      res.send(err);
+    } else if (docs[0] === undefined) {
+      // res.status(404).send('Database does not contain requested record.');
+      fullResponse.sidebar({ notFound: true });
+    } else {
+      fullResponse.sidebar(docs[0]);
+    }
+  });
+  db.getPreviewVideo(req.query, (err, docs) => {
+    if (err) {
+      res.send(err);
+    } else if (docs[0] === undefined) {
+      // res.status(404).send('Database does not contain requested record.');
+      fullResponse.previewVideo({ notFound: true });
+    } else {
+      fullResponse.previewVideo(docs[0]);
+    }
+  });
 });
 
 module.exports = app;
