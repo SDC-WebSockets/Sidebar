c0fc93e9ac109eb444e8c3b24085b0831156345c
kimberly kung <kimberly.kung@gmail.com>
2021-06-25 19:11:50 -0700

    starting create for sidebar

--- database/database.js
+++ database/database.js
@@ -52,10 +52,31 @@ const getPreviewVideo = (query, callback) => PreviewVideo.find(query, callback);
 const getSidebar = (query, callback) => Sidebar.find(query, callback);
 
 // ------------------------------------------------------------------
+// Functions that create information to the database
+const postPrice = (query, callback) => Price.find(query, callback);
+
+const postPreviewVideo = (query, callback) => PreviewVideo.find(query, callback);
+
+const postSidebar = async (newDoc) => {
+  // check if courseId already exists
+  await Sidebar.find({ courseId: newDoc.courseId })
+    .then((foundDoc) => {
+      if (foundDoc) {
+        return { exists: true };
+      }
+      return { exists: false };
+    })
+    .
+};
+
+// ------------------------------------------------------------------
 // Exports
 exports.getPrice = getPrice;
 exports.getSidebar = getSidebar;
 exports.getPreviewVideo = getPreviewVideo;
+exports.postPrice = postPrice;
+exports.postSidebar = postSidebar;
+exports.postPreviewVideo = postPreviewVideo;
 exports.Price = Price;
 exports.PreviewVideo = PreviewVideo;
 exports.Sidebar = Sidebar;
