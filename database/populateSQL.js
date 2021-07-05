4dbcb938f269b2efb6abedddf672c10a9583a2ab
kimberly kung <kimberly.kung@gmail.com>
2021-07-04 20:24:07 -0700

    FINAL POSTGRESS SEED SCRIPT.
    that took forever, and now i know thousands of ways to read a file.

--- database/populateSQL.js
+++ database/populateSQL.js
@@ -1,10 +1,4 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
-
-const fs = require('fs');
-const readline = require('readline');
-const { once } = require('events');
 const path = require('path');
 const { Pool } = require('pg');
 
@@ -17,56 +11,15 @@ const pricePath = path.join(`${__dirname}/data_gen/priceData.csv`);
 const videoPath = path.join(`${__dirname}/data_gen/videoData.csv`);
 const sidebarPath = path.join(`${__dirname}/data_gen/sidebarData.csv`);
 
-const readCSV = async (pathToRead, type) => {
-  let linesRead = 0;
-  let keys = [];
-  return new Promise((resolve, reject) => {
-    const stream = fs.createReadStream(pathToRead, { encoding: 'utf8' }).pause();
-    const rl = readline.createInterface({
-      input: stream,
-      // crlfDelay: Infinity,
-    });
-
-    rl.on('line', async (line) => {
-      const lineArray = line.split(',');
-      if (linesRead === 0) {
-        keys = lineArray;
-        console.log(keys);
-        linesRead += 1;
-      } else {
-        // console.log(linesRead);
-        const db = { name: type };
-        if (type === 'price') {
-          db.cols = 'courseId, basePrice, discountPercentage, saleNumOfDays, saleOngoing';
-          db.numCols = '$1, $2, $3, $4, $5';
-        }
-        if (type === 'video') {
-          db.cols = 'courseId, videoImgUrl, videoImg';
-          db.numCols = '$1, $2, $3';
-        }
-        if (type === 'sidebar') {
-          db.cols = 'courseId, fullLifetimeAccess, assignments, certificateOfCompletion, downloadableResources';
-          db.numCols = '$1, $2, $3, $4, $5';
-        }
-        const insertText = `INSERT INTO ${db.name}(${db.cols}) VALUES(${db.numCols})`;
-        await pool.query(insertText, lineArray)
-          .then(() => {
-            linesRead += 1;
-          })
-          .catch((err) => {
-            console.log(`error saving to DB at ${linesRead}:`, err);
-            reject(err);
-          });
-      }
-    }).on('close', () => {
-      console.log('Done Seeding ', type);
-      stream.close();
-      resolve();
-    });
-  });
+const copyCSV = (pathToCopy, type) => {
+  const insertText = `COPY ${type}
+  FROM '${pathToCopy}'
+  DELIMITER ','
+  CSV HEADER;`;
+  return pool.query(insertText);
 };
 
-(async () => Promise.resolve(pool.connect())
+const seedPSQL = async () => Promise.resolve(pool.connect())
   .then(() => pool.query('SELECT NOW()'))
   .then((result) => {
     console.log('DB connected at ', result.rows[0].now);
@@ -96,15 +49,28 @@ const readCSV = async (pathToRead, type) => {
   })
   .then(() => {
     console.log('Table Creations SUCCESS!');
-    return readCSV(pricePath, 'price');
+    return copyCSV(pricePath, 'price');
   })
-  // .then(() => readCSV(videoPath, 'video'))
-  // .then(() => readCSV(sidebarPath, 'sidebar'))
-  .then(() => {
-    console.log('Completed All Seeding!');
+  .then(() => pool.query('SELECT NOW()'))
+  .then((result) => {
+    console.log('Finished seeding price at ', result.rows[0].now);
+    return copyCSV(videoPath, 'video');
+  })
+  .then(() => pool.query('SELECT NOW()'))
+  .then((result) => {
+    console.log('Finished seeding video at ', result.rows[0].now);
+    return copyCSV(sidebarPath, 'sidebar');
+  })
+  .then(() => pool.query('SELECT NOW()'))
+  .then((result) => {
+    console.log('Completed All Seeding at ', result.rows[0].now);
     return pool.end();
+  })
+  .then(() => {
+    console.log('bye!');
   })
   .catch((error) => {
     console.warn('Error occured: ', error);
-  })
-)();
+  });
+
+seedPSQL();
