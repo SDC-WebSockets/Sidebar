cdad01b61dfea2672064cacb09bc82d5c0adda36
kimberly kung <kimberly.kung@gmail.com>
2021-07-04 18:56:18 -0700

    attempt at async seeding

--- database/populateSQL.js
+++ database/populateSQL.js
@@ -2,20 +2,19 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
 
-// const { Sequelize, Model, DataTypes } = require('sequelize');
 const fs = require('fs');
 const readline = require('readline');
+const { once } = require('events');
 const path = require('path');
-const {
-  Price,
-  PreviewVideo,
-  Sidebar,
-  openConn,
-  closeConn,
-} = require('./pSQLdatabase.js');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'SDC',
+});
 
 const pricePath = path.join(`${__dirname}/data_gen/priceData.csv`);
-const previewVideoPath = path.join(`${__dirname}/data_gen/previewVideoData.csv`);
+const videoPath = path.join(`${__dirname}/data_gen/videoData.csv`);
 const sidebarPath = path.join(`${__dirname}/data_gen/sidebarData.csv`);
 
 const priceObj = (keys, array) => {
@@ -39,52 +38,92 @@ const priceObj = (keys, array) => {
   return newPrice;
 };
 
-const readCSV = (pathToRead, type) => {
+const readCSV = async (pathToRead, type) => {
   let linesRead = 0;
   let keys = [];
+  try {
+    const stream = fs.createReadStream(pathToRead, { encoding: 'utf8' }).pause();
+    const rl = readline.createInterface({
+      input: stream,
+      // crlfDelay: Infinity,
+    });
 
-  const stream = fs.createReadStream(pathToRead, { encoding: 'utf8' }).pause();
-  const rl = readline.createInterface({
-    input: stream,
-  });
-
-  rl.on('line', async (line) => {
-    const lineArray = line.split(',');
-    if (linesRead === 0) {
-      keys = lineArray;
-      console.log(keys);
-    } else {
-      console.log(linesRead);
-      let newData = {};
-      if (type === 'price') {
-        newData = priceObj(keys, lineArray);
-        Price.create(newData);
+    rl.on('line', async (line) => {
+      const lineArray = line.split(',');
+      if (linesRead === 0) {
+        keys = lineArray;
+        console.log(keys);
+      } else {
+        // console.log(linesRead);
+        const db = { name: type };
+        if (type === 'price') {
+          db.cols = 'courseId, basePrice, discountPercentage, saleNumOfDays, saleOngoing';
+          db.numCols = '$1, $2, $3, $4, $5';
+        }
+        if (type === 'video') {
+          db.cols = 'courseId, videoImgUrl, videoImg';
+          db.numCols = '$1, $2, $3';
+        }
+        if (type === 'sidebar') {
+          db.cols = 'courseId, fullLifetimeAccess, assignments, certificateOfCompletion, downloadableResources';
+          db.numCols = '$1, $2, $3, $4, $5';
+        }
+        const insertText = `INSERT INTO ${db.name}(${db.cols}) VALUES(${db.numCols})`;
+        console.log(insertText)
+        await pool.query(insertText, lineArray)
+          .then(() => {
+            linesRead += 1;
+          })
+          .catch((err) => {
+            console.log(`error saving to DB at ${linesRead}:`, err);
+          });
       }
-    }
-    linesRead += 1;
-  });
+    });
+    await once(rl, 'close');
+    console.log('Done Seeding ', type);
+    stream.close();
+  } catch (error) {
+    console.warn(`ERROR reading ${type}: ${error}`);
+  }
 };
 
-const postgresDataGen = async (numberOfCourses) => {
-  console.log(`Populating DB with ${numberOfCourses} records`);
-  readCSV(pricePath, 'price');
-};
-
-const numRecsGenerating = 10 ** 7;
-(async () => Promise.resolve(openConn())
-  .then(async () => {
-    await postgresDataGen(numRecsGenerating);
-    // return Promise.all([Price.findAll(), PreviewVideo.findAll(), Sidebar.findAll()]);
-    return Price.count();
+(async () => Promise.resolve(pool.connect())
+  .then(() => pool.query('SELECT NOW()'))
+  .then((result) => {
+    console.log('DB connected at ', result.rows[0].now);
+    const createTables = `DROP TABLE IF EXISTS price;
+      CREATE TABLE price(
+        courseId SERIAL PRIMARY KEY,
+        basePrice INTEGER,
+        discountPercentage INTEGER,
+        saleNumOfDays INTEGER,
+        saleOngoing BOOLEAN
+      );
+      DROP TABLE IF EXISTS video;
+      CREATE TABLE video(
+        courseId SERIAL PRIMARY KEY,
+        videoImgUrl VARCHAR(30),
+        videoImg VARCHAR(30)
+      );
+      DROP TABLE IF EXISTS sidebar;
+      CREATE TABLE sidebar(
+        courseId SERIAL PRIMARY KEY,
+        fullLifetimeAccess BOOLEAN,
+        assignments BOOLEAN,
+        certificateOfCompletion BOOLEAN,
+        downloadableResources INTEGER
+      );`;
+    return pool.query(createTables);
   })
-  .then((results) => {
-    console.log('Requested', numRecsGenerating, 'records to be generated.');
-    console.log('price count: ', results);
-    // console.log('Price, Preview Video, Sidebar DB sizes respectively.');
-    // results.forEach((result) => {
-    //   console.log(result.length);
-    // });
-    // closeConn();
+  .then(() => {
+    console.log('Table Creations SUCCESS!');
+    return readCSV(pricePath, 'price');
+  })
+  .then(() => readCSV(videoPath, 'video'))
+  .then(() => readCSV(sidebarPath, 'sidebar'))
+  .then(() => {
+    console.log('Completed All Seeding!');
+    return pool.end();
   })
   .catch((error) => {
     console.warn('Error occured: ', error);
