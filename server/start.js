184fa70f9da95ff67112bfdda08f21031d48117f
EC2 Default User <ec2-user@ip-172-31-27-60.us-west-2.compute.internal>
2021-08-10 02:35:31 +0000

    Updated port and private host now that I understand it..

--- server/start.js
+++ server/start.js
@@ -3,10 +3,10 @@ const app = require('./server.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3004;
-// const host = process.env.PRIVATE_HOST || 'localhost'
+const host = process.env.PRIVATE_HOST || 'localhost'
 
-const serverInstance = app.listen(port, () => {
-  console.log(`Listening at ${port}`)
+const serverInstance = app.listen(port, host, () => {
+  console.log(`Listening at ${host}:${port}`)
 });
 
 const closeServer = () => {
